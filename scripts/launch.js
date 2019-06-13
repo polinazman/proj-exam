5c11e16850f8696aee224e6259a979832b45b4b4
polinazman <polina.j4@gmail.com>
2019-06-13 23:22:32 +0500

    Script file added

---
+++ scripts/launch.js
@@ -0,0 +1,53 @@
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://api.spacexdata.com/v3/launches/next', true)
+
+request.onload = function() {
+	var launch = JSON.parse(request.response)
+			console.log(launch.launch_date_utc)
+
+			var nextLaunch = document.getElementById("launch-countdown");
+
+			var utcDate = new Date(launch.launch_date_utc)
+
+			var timer = setInterval(function() {
+				var now = new Date().getTime();
+				var t = utcDate - now;
+
+				if (t >= 0) {
+					var days = Math.floor(t / (1000 * 60 * 60 * 24));
+					var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+					var mins = Math.floor((t % (1000 * 60 * 60)) / (1000 *60));
+					var secs = Math.floor((t % (1000 * 60 )) / 1000);
+
+					document.getElementById("timer").innerHTML = days + "d " + hours + "h " + mins + "m " + secs + "s ";
+
+					var timer = document.getElementById("timer");
+					
+					var timerDays = document.createElement("timer-days");
+					timerDays.setAttribute("class", "timer-days");
+					timer.appendChild(timerDays);
+					timerDays.innerHTML = days + "d ";
+
+					var timerHours = document.createElement("timer-hours");
+					timerHours.setAttribute("class", "timer-hours");
+					timer.appendChild(timerHours);
+					timerHours.innerHTML = hours + "h ";
+
+					var timerMins = document.createElement("timer-mins");
+					timerMins.setAttribute("class", "timer-mins");
+					timer.appendChild(timerMins);
+					timerMins.innerHTML = mins + "m ";
+
+					var timerSecs = document.createElement("timer-secs");
+					timerSecs.setAttribute("class", "timer-secs");
+					timer.appendChild(timerSecs);
+					timerSecs.innerHTML = secs + "s ";
+					
+				} else {
+					document.getElementById("timer").innerHTML = "The launch starts now!";
+				}
+			}, 1000);
+}
+
+request.send()

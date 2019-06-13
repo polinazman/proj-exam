2ed6adba81a1cc4b1aa301faf9fcb9ed528313f9
polinazman <polina.j4@gmail.com>
2019-06-13 17:39:32 +0500

    News api added

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,43 @@
-var request URL = "https://spaceflightnewsapi.net/api/v1/articles?news_site=spacex";
+window.onload = makeApiCall;
 
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
+function makeApiCall(e, title) {
+    var requestUrl = "https://spaceflightnewsapi.net/api/v1/articles?search=spacex";
+
+    var request = new XMLHttpRequest();
+    request.open("GET", requestUrl);
+
+    request.responseType = "json";
+
+    request.send();
+
+    request.onload = (e) => fillPage(request.response);
+}
+
+function fillPage(myArticle) {
+	myArticle.docs.forEach(doc => {
+
+		var docs = document.getElementById("article");
+
+		var outerDiv = document.createElement("div");
+		outerDiv.setAttribute("class", "col1");
+		docs.appendChild(outerDiv);
+
+		var innerDiv = document.createElement("div");
+		innerDiv.setAttribute("class", "article-container");
+		outerDiv.appendChild(innerDiv);
+
+		var img = document.createElement('img');
+		img.setAttribute('src', doc.featured_image);
+		innerDiv.appendChild(img);
+
+		var textbox = document.createElement("div");
+		textbox.setAttribute("class", "textbox");
+		innerDiv.appendChild(textbox);
+
+		var a = document.createElement('a');
+		a.setAttribute('href', doc.url);
+		textbox.appendChild(a);
+		a.innerHTML = doc.title;
+
+	});
+}

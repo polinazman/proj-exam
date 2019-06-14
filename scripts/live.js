000ac2a5605a468402e70b182660019b1124b3ef
polinazman <polina.j4@gmail.com>
2019-06-14 18:06:38 +0500

    JS file added

---
+++ scripts/live.js
@@ -0,0 +1,24 @@
+var request = new XMLHttpRequest()
+
+request.open('GET', 'http://api.open-notify.org/astros.json', true)
+
+request.onload = function() {
+	var livestats = JSON.parse(request.response)
+
+		var numberOfPeople = livestats.number
+
+		document.getElementById("number").innerHTML = numberOfPeople;
+
+		var i = numberOfPeople
+
+		for ( i = 0; i < livestats.people.length; i++) {
+			var names = livestats.people[i].name;
+
+			var outputNames = names.toString();
+
+			document.getElementById("people").innerHTML += outputNames + ", ";
+
+		}
+}
+
+request.send()
